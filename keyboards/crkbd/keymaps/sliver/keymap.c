5f4bf381f245492d27756e77218fcb9a42c54a35
João Inez <joaoinez.info@gmail.com>
2025-07-31 16:37:36 +0200

    refactor: swap tab/esc and super/alt

--- keyboards/crkbd/keymaps/sliver/keymap.c
+++ keyboards/crkbd/keymaps/sliver/keymap.c
@@ -31,18 +31,18 @@ enum layers {
 #define MO_SYM MO(_SYM)
 #define MO_FUNC MO(_FUNC)
 
-#define CTL_TAB CTL_T(KC_TAB)
+#define CTL_ESC CTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      CTL_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_BSLS,
+      CTL_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          MO_FUNC,  MO_SYM,  KC_SPC,     KC_ENT, KC_RALT, KC_RGUI
+                                          MO_FUNC,  MO_SYM,  KC_SPC,     KC_ENT, KC_RGUI, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 

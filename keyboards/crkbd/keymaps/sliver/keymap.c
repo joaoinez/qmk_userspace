75a6dd06f7fd8393a996caf55607b7e9d88636ae
João Inez <joaoinez.info@gmail.com>
2025-12-09 06:56:38 +0100

    fix: deprecated keymaps

--- keyboards/crkbd/keymaps/sliver/keymap.c
+++ keyboards/crkbd/keymaps/sliver/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      CTL_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_BSLS,
+      CTL_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          MO_FUNC,  MO_SYM,  KC_SPC,     KC_ENT, KC_LGUI, KC_LALT
+                                          KC_LALT, KC_LGUI,  KC_SPC,     KC_SPC,  MO_SYM, MO_FUNC
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       CW_TOGG,  KC_GRV, KC_MINS, KC_LBRC, KC_RBRC,  KC_EQL,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_SCLN, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, _______, _______, _______, _______,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, _______, _______, KC_BSLS, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, XXXXXXX, _______,    _______, _______, _______
+                                          _______, _______, _______,    _______, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -62,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_HUI,  KC_F11,  KC_F12, RGB_SAI, RGB_VAI, RGB_SPI,                      KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT, KC_PSCR, KC_HOME,
+      UG_HUEU,  KC_F11,  KC_F12, UG_SATU, UG_VALU, UG_SPDU,                      KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT, KC_PSCR, KC_HOME,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_HUD, RGB_TOG, RGB_MOD, RGB_SAD, RGB_VAD, RGB_SPD,                      XXXXXXX, KC_MUTE, XXXXXXX, KC_MPRV, XXXXXXX, XXXXXXX,
+      UG_HUED, UG_TOGG, UG_NEXT, UG_SATD, UG_VALD, UG_SPDD,                      XXXXXXX, KC_MUTE, XXXXXXX, KC_MPRV, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
